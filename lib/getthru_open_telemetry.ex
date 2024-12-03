4e7661f59b82defc9c1b8bc3b5cb4212b2b72618
Matt <matt@silbernagel.dev>
2024-12-03 08:45:29 -0500

    initial creation of project

---
+++ lib/getthru_open_telemetry.ex
@@ -0,0 +1,18 @@
+defmodule GetthruOpenTelemetry do
+  @moduledoc """
+  Documentation for `GetthruOpenTelemetry`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GetthruOpenTelemetry.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/getthru_open_telemetry_test.exs
@@ -0,0 +1,8 @@
+defmodule GetthruOpenTelemetryTest do
+  use ExUnit.Case
+  doctest GetthruOpenTelemetry
+
+  test "greets the world" do
+    assert GetthruOpenTelemetry.hello() == :world
+  end
+end
